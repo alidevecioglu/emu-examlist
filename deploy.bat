fb68a530cb9a490b47bc5abfee5b8d7105056891
Ali <alidevecioglu12324@gmail.com>
2024-03-16 17:47:25 +0200

    Deploy Script Update

--- deploy.bat
+++ deploy.bat
@@ -1,30 +1,20 @@
-
 @echo off
-SETLOCAL
+SETLOCAL EnableDelayedExpansion
 
-REM Read the .env file and extract the values
-for /f "delims=" %%a in ('type .env') do (
-    set "line=%%a"
-    if "!line:~0,10!"=="CAPROVER_APP_TOKEN=" set "appToken=!line:~10!"
-    if "!line:~0,13!"=="CAPROVER_URL=" set "caproverUrl=!line:~13!"
-    if "!line:~0,13!"=="CAPROVER_APP=" set "caproverApp=!line:~13!"
-)
+CALL env.bat
 
 REM Get the name of the current directory to use as the tar file name
 FOR %%I IN (.) DO SET CurrentDirName=%%~nxI
 
-CD SRC
+REM Change into the SRC directory to create the tar file
+CD SRC || exit /b
 
 REM Create a tar file excluding node_modules and .vscode directories
-tar --exclude=node_modules --exclude=.vscode --exclude=%CurrentDirName%.tar --exclude=deploy.bat -cvf %CurrentDirName%.tar *
-
-REM Move the tar file to the parent folder
-move %CurrentDirName%.tar ..
+tar --exclude=node_modules --exclude=.vscode --exclude=..\\%CurrentDirName%.tar --exclude=deploy.bat -cvf ..\\%CurrentDirName%.tar *
 
 CD ..
 
 REM Deploy using the extracted values
-caprover deploy --tarFile %CurrentDirName%.tar --appToken %appToken% --caproverUrl %caproverUrl% --caproverApp %caproverApp%
+caprover deploy --tarFile %CurrentDirName%.tar --appToken %CAPROVER_APP_TOKEN% --caproverUrl %CAPROVER_URL% --caproverApp %CAPROVER_APP%
 
 PAUSE
-
